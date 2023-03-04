1af8790859c1fb7d9ad332f2166787fc894127c5
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-03-04 14:53:58 +0700

    SceneView

--- src/views/SceneView.java
+++ src/views/SceneView.java
@@ -1,5 +1,20 @@
 package views;
 
-public abstract class SceneView {
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import main.MainApplication;
 
+public abstract class SceneView {
+	
+	protected StackPane sp;
+	protected Scene scene;
+	protected MainApplication app;
+	
+	public abstract void initComponents();
+	
+	public abstract void addComponents();
+	
+	public abstract void initScene();
+	
+	
 }
