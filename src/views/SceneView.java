a53a89c8fa540c52a90bfe018388cfbaee9326f1
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-03-05 08:52:05 +0700

    SceneView done

--- src/views/SceneView.java
+++ src/views/SceneView.java
@@ -1,5 +1,7 @@
 package views;
 
+
+import controllers.SoundController;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import main.MainApplication;
@@ -8,13 +10,37 @@ public abstract class SceneView {
 	
 	protected StackPane sp;
 	protected Scene scene;
+	
+	protected SoundController sc;
 	protected MainApplication app;
 	
 	public abstract void initComponents();
 	
-	public abstract void addComponents();
+	public abstract StackPane addComponents();
 	
-	public abstract void initScene();
+	public abstract Scene initScene();
 	
+	public void generateScene(MainApplication app) {
+		this.sc = SoundController.getInstance();
+		this.app = app;
+		this.initComponents();
+		this.sp = this.addComponents();
+		this.scene = this.initScene();
+	}
 	
+	public StackPane getStackPane() {
+		return sp;
+	}
+
+	public void setStackPane(StackPane sp) {
+		this.sp = sp;
+	}
+
+	public Scene getScene() {
+		return scene;
+	}
+
+	public void setScene(Scene scene) {
+		this.scene = scene;
+	}
 }
