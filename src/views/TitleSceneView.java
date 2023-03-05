981656f8a76336eb3be7d89e942408bbf7e78b67
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-03-05 08:52:11 +0700

    TitleSceneView

---
+++ src/views/TitleSceneView.java
@@ -0,0 +1,26 @@
+package views;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class TitleSceneView extends SceneView {
+
+	@Override
+	public void initComponents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public StackPane addComponents() {
+		// TODO Auto-generated method stub
+		return sp;
+	}
+
+	@Override
+	public Scene initScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
